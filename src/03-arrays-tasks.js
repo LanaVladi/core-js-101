e6fdbbb5190a869867dd5be12763dcc84b8bbca4
Svetlana <lanavladi1605gmail.com>
2022-07-24 16:48:24 +0500

    feat: add solution 03-arrays-tasks.js

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -241,13 +241,11 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
-  // const arr2 = arr.map(function(el) {
-  //   let initVal = 0;
-  //   initVal += el;
-  //   return arr2;
-  //  });
+function getMovingSum(arr) {
+  // throw new Error('Not implemented');
+  let sum = 0;
+  const resultArr = arr.map((el) => (sum += el));
+  return resultArr;
 }
 
 /**
